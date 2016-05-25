238dbb2bf53deef45d0efd6ec0f78265519fab99
Caitlin MacMinn <macminn.caitlin@gmail.com>
2016-05-25 12:46:19 -0400

    Working on files

--- js/test.js
+++ js/test.js
@@ -1,6 +1,16 @@
 var app = {};
 
-app.getData = function() {
+var location = {};
+
+petApp.init = function() {
+	petApp.getLocationData();
+
+	var location = $('select').val();
+	movieDB.getDataByGenre(genreID);
+	// console.log(genreID);
+}
+
+petApp.getLocationData = function() {
 	$.ajax({
 		url: 'http://api.petfinder.com/shelter.find',
 		type: 'GET',
@@ -9,16 +19,17 @@ app.getData = function() {
 			key: '7650ccca5ad807a0a39eaf4aed5ccb10',
 			location:'l1a4c5',
 			format:'json',
-			count:5 // This will help limit the number of shelters. The first object returned is closest location
+			// count:5 // This will help limit the number of shelters. The first object returned is closest location
 			}
 	}).then(function(results){
 			console.log(results.petfinder.shelters.shelter);
 		});
 	};
 
+petApp.
 
 $(function(){
-	app.getData();
+	petApp.init();
 });
 
 
